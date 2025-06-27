b194244a4ce98c2702efa1764be7cd5cd27ba852
leaderman <miao.work2010@gmail.com>
2025-06-27 17:10:07 +0800

    添加 lombok

--- domain/src/main/java/io/github/njdi/javacamp/domain/request/ParameterRequest.java
+++ domain/src/main/java/io/github/njdi/javacamp/domain/request/ParameterRequest.java
@@ -1,51 +1,16 @@
 package io.github.njdi.javacamp.domain.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class ParameterRequest {
   private Long id;
   private String name;
   private BigDecimal price;
   private Boolean enabled;
   private String[] tags;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public BigDecimal getPrice() {
-    return price;
-  }
-
-  public void setPrice(BigDecimal price) {
-    this.price = price;
-  }
-
-  public Boolean getEnabled() {
-    return enabled;
-  }
-
-  public void setEnabled(Boolean enabled) {
-    this.enabled = enabled;
-  }
-
-  public String[] getTags() {
-    return tags;
-  }
-
-  public void setTags(String[] tags) {
-    this.tags = tags;
-  }
 }
